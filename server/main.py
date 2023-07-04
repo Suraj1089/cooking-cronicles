76fb184a471a5166fda218fd898e9c091f7ba347
Suraj Pisal <surajpisal113@gmail.com>
2023-07-04 20:35:14 +0530

    add routes for contact,blogs and menus

--- server/main.py
+++ server/main.py
@@ -11,6 +11,26 @@ app.mount("/static", StaticFiles(directory="frontend"), name="static")
 templates = Jinja2Templates(directory="frontend")
 
 
+links = [
+    {"label": "Home", "url": "index.html"},
+    {"label": "Our Menus", "url": "menu.html"},
+    {"label": "Blog Entries", "url": "blog.html"},
+    {"label": "Contact Us", "url": "contact.html"},
+]
+
+
 @app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "id": id})
+async def Home(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request, "links": links})
+
+@app.get("/contact", response_class=HTMLResponse)
+async def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request, "links": links})
+
+@app.get("/blogs", response_class=HTMLResponse)
+async def blogs(request: Request):
+    return templates.TemplateResponse("blog.html", {"request": request, "links": links})
+
+@app.get("/menus", response_class=HTMLResponse)
+async def menu(request: Request):
+    return templates.TemplateResponse("menu.html", {"request": request, "links": links})
